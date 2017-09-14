4535c97578b8b1eef267f90eb86be1b458368f4b
ijm7 <ijmorton500@gmail.com>
2017-09-14 12:29:17 +0100

    Boilerplate for Outlet Controller

---
+++ app/Http/Controllers/OutletsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OutletsController extends Controller
+{
+	// Function for showing returning a specific outlet
+    public function show(Outlet $outlet)
+    {
+		
+	}
+}
