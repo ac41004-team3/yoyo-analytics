cc062e122fcca37a6c1190c535eda52bcbd1af01
ijm7 <ijmorton500@gmail.com>
2017-09-14 13:44:59 +0100

    This branch is setup for experimenting with data representation from the database

--- app/Http/Controllers/OutletsController.php
+++ app/Http/Controllers/OutletsController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Outlet;
 
 class OutletsController extends Controller
 {
 	// Function for showing returning a specific outlet
-    public function show(Outlet $outlet)
-    {
-		
+	public function index()
+	{
+		$outlets = Outlet::all();
+		return view('outlets.index', compact('outlets'));
 	}
+	
+    /*public function show(Outlet $outlet)
+    {
+		$outlet = Outlet::find($id);
+	}*/
 }
