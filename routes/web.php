90511b68401c5a8388010b8cdff3d9f006233643
ijm7 <ijmorton500@gmail.com>
2017-09-14 14:45:05 +0100

    Added basic controllers for Outlets, Transactions and Customers

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/outlets','TaskController@index');
+Route::get('/outlets','OutletsController@index');
+Route::get('/transactions', 'TransactionsController@index');
+Route::get('/customers', 'CustomersController@index');
