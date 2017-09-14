52476f50bdc48991b563c52a57e8860e3302a3e1
James Neill <jneill542@gmail.com>
2017-09-14 22:52:35 +0100

    Uploaded file validated and imported

--- app/Listeners/ProcessUploadedFile.php
+++ app/Listeners/ProcessUploadedFile.php
@@ -2,9 +2,11 @@
 
 namespace App\Listeners;
 
+use App\Customer;
 use App\Events\FileUploaded;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Outlet;
+use App\Transaction;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 
 class ProcessUploadedFile
@@ -28,9 +30,37 @@ class ProcessUploadedFile
     public function handle(FileUploaded $event)
     {
         Log::debug('Starting import..');
-        foreach (file(storage_path('app/' . $event->path)) as $no => $line) {
-            Log::debug($no . '|  ' . $line);
+        foreach (file(storage_path('app/' . $event->path)) as $data) {
+            $transaction = $this->process($data);
+            Transaction::create($transaction);
         }
         Log::debug('Import finished!');
     }
+
+    private function process($data)
+    {
+        $data = explode(';', $data);
+
+        $customer = Customer::updateOrCreate(['id' => $data[5]]);
+        $outlet = Outlet::updateOrCreate(['id' => $data[2]], [
+            'name' => $data[4]
+        ]);
+
+        Log::debug($this->parseCurrency($data[7]));
+        return [
+            'customer_id' => $customer->id,
+            'outlet_id' => $outlet->id,
+            'date' => new Carbon(str_replace('/', '-', $data[0])),
+            'type' => $data[6],
+            'spent' => $this->parseCurrency($data[7]),
+            'discount' => $this->parseCurrency($data[8]),
+            'total' => $this->parseCurrency($data[9]),
+        ];
+    }
+
+    private function parseCurrency($value)
+    {
+        return preg_replace('/[^0-9]/', '', $value);
+    }
+
 }
