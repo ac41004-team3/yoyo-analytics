14b5d590fecedcb958bdfd3dad165f8e26d082a4
dbrereton1995 <dbrereton1995@gmail.com>
2017-09-14 14:17:00 +0100

    Analytics page base laid out

---
+++ resources/views/analytics.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container-fluid">
+    
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-2">     
+            <u><p><h1>Charts:</h1> Select your chart</p></u>
+            <div class="col-sm-3 ">
+                one
+            </div>
+            
+            <div class="col-sm-3">
+                two
+            </div>
+                
+            <div class="col-sm-3">
+              three  
+            </div>
+            
+            <div class="col-sm-3">
+              four  
+            </div>
+        </div>
+    </div>
+    
+    <div class="row">
+        <div class="col-sm-10 col-sm-offset-2">
+            
+            <u><p><h1>Options:</h1> Customise your chart</p></u>
+            
+         <div class="row">
+             <div class="col-sm-12">
+                 <p><b>Outlets</b></p>
+             </div>
+             
+            <div class="col-sm-3 ">
+                one
+            </div>
+            
+            <div class="col-sm-3">
+                two
+            </div>
+                
+            <div class="col-sm-3">
+              three  
+            </div>
+            
+            <div class="col-sm-3">
+              four  
+            </div>
+         </div>
+            </br>
+            <div class="row">
+                
+             <div class="col-sm-12">
+                 <p><b>Tribes:</b></p>
+             </div>
+                
+            <div class="col-sm-3 ">
+                one
+            </div>
+            
+            <div class="col-sm-3">
+                two
+            </div>
+                
+            <div class="col-sm-3">
+              three  
+            </div>
+            
+            <div class="col-sm-3">
+              four  
+            </div>
+         </div>
+            
+        </div>
+    </div>
+    
+    
+</div>
+
+@endsection
