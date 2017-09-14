e98375094c78f503423b37c6d5f9aa2aaea607bb
dbrereton1995 <dbrereton1995@gmail.com>
2017-09-14 15:34:20 +0100

    updated analytics page with prototype system where hovering over an option executes an enlarging effect. General structure confirmed.

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -6,48 +6,56 @@
     
     <div class="row">
         <div class="col-sm-10 col-sm-offset-2">     
-            <u><p><h1>Charts:</h1> Select your chart</p></u>
-            <div class="col-sm-3 ">
-                one
+            <p><h1><u>Charts</u>:</h1> Select your chart</p>
+        <div class="row">
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Line Graph</p>
             </div>
             
-            <div class="col-sm-3">
-                two
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Bar Chart</p>
             </div>
                 
-            <div class="col-sm-3">
-              three  
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Pie Chart</p> 
             </div>
             
-            <div class="col-sm-3">
-              four  
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Spatial Chart</p>
             </div>
         </div>
+        </div>
     </div>
+    
+    </br>
     
     <div class="row">
         <div class="col-sm-10 col-sm-offset-2">
             
-            <u><p><h1>Options:</h1> Customise your chart</p></u>
+            <p><h1><u>Options</u>:</h1> Customise your chart</p>
             
          <div class="row">
              <div class="col-sm-12">
                  <p><b>Outlets</b></p>
              </div>
              
-            <div class="col-sm-3 ">
+            <div class="col-sm-3 selectionBox">
                 one
             </div>
             
-            <div class="col-sm-3">
+            <div class="col-sm-3 selectionBox">
                 two
             </div>
                 
-            <div class="col-sm-3">
+            <div class="col-sm-3 selectionBox">
               three  
             </div>
             
-            <div class="col-sm-3">
+            <div class="col-sm-3 selectionBox">
               four  
             </div>
          </div>
@@ -55,23 +63,27 @@
             <div class="row">
                 
              <div class="col-sm-12">
-                 <p><b>Tribes:</b></p>
+                 <p><b>Tribes</b></p>
              </div>
                 
-            <div class="col-sm-3 ">
-                one
+            <div class="col-sm-3 selectionBox">
+                <img src="{{ URL::asset('/images/whale.jpg')}}">
+                <p>Whales</p>
             </div>
             
-            <div class="col-sm-3">
-                two
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Early Birds</p>
             </div>
                 
-            <div class="col-sm-3">
-              three  
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Night Owls</p>  
             </div>
             
-            <div class="col-sm-3">
-              four  
+            <div class="col-sm-3 selectionBox">
+                <img src="">
+                <p>Creatures of Habit</p> 
             </div>
          </div>
             
