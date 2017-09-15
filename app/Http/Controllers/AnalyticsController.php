d5f763c3d4e47ab9cfe04c0e2049836f357c4c00
dbrereton1995 <dbrereton1995@gmail.com>
2017-09-15 11:17:08 +0100

    updated name of AnalyticsController class

--- app/Http/Controllers/AnalyticsController.php
+++ app/Http/Controllers/AnalyticsController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class analyticsController extends Controller
+class AnalyticsController extends Controller
 {
      /**
      * Show the application dashboard.
