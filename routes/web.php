f8f050761cba4d77db40d91cb164efbe7f68c5f2
ijm7 <ijmorton500@gmail.com>
2017-09-15 16:11:43 +0100

    Progress to a chart that will show transactions over a period of time

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/outlets', 'OutletsController@index');
+Route::get('/outlets/{outlet}', 'OutletsController@show');
+Route::get('/transactions/{transaction}', 'TransactionsController@show');
+Route::get('outlets/{outlet}/chart', 'OutletsController@chart');
