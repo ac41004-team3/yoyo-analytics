6348d9c146a57ea280f55da2be229fad71da43c6
ijm7 <ijmorton500@gmail.com>
2017-09-17 19:18:35 +0100

    Functionality for random chart colours added

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -9,9 +9,13 @@
     <button id="Bar" onclick="changeChartType('bar')">Bar</button>
     <button id="Pie" onclick="changeChartType('pie')">Pie</button>
     <button id="Spatial" onclick="changeChartType('radar')">Spatial</button>
+    <br>
     <div style="width:75%; height:50%;">
         <canvas id="myChart" width="100" height="100"></canvas>
         <script>
+        var currentChartData = {
+
+        }
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, { //PlaceHolder Chart
             type: 'bar',
@@ -21,22 +25,14 @@
                     label: '# of Votes',
                     data: [12, 19, 3, 5, 2, 3],
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        getRandomColor(),
+                        getRandomColor(),
+                        getRandomColor(),
+                        getRandomColor(),
+                        getRandomColor(),
+                        getRandomColor()
                     ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
+                    borderWidth: 0
                 }]
             },
             options: {
@@ -54,6 +50,14 @@
             myChart = new Chart(ctx, {
                 type: chartType
             });
+        }
+        function getRandomColor() {
+            var letters = '0123456789ABCDEF';
+            var color = '#';
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
         }
         </script>
     </div>
