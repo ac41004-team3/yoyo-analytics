5c6ddefcdbdc6ffa35e41343ddce3763f24102b5
ijm7 <ijmorton500@gmail.com>
2017-09-17 19:58:57 +0100

    Added enumeration for outlet IDs

--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -13,9 +13,28 @@
     <div style="width:75%; height:50%;">
         <canvas id="myChart" width="100" height="100"></canvas>
         <script>
-        var currentChartData = {
-
-        }
+        var outletID = {    DJCAD_CANTINA: 235,
+                            AIR_BAR: 236,
+                            FLOOR_FIVE: 237,
+                            LIBRARY: 238,
+                            DENTAL_CAFE: 239,
+                            FOOD_ON_FOUR: 240,
+                            LIAR_BAR: 241,
+                            MONO: 242,
+                            LEVEL_2_RECEPTION: 243,
+                            PREMIER_SHOP_YOYO_ACCEPT: 343,
+                            DUSA_THE_UNION_MARKETPLACE:456,
+                            PREMIER_SHOP: 2676,
+                            COLLEGE_SHOP: 2677,
+                            NINEWELLS_SHOP: 2679
+                        };
+        //Basically an enum and can be treated syntaxically as such,
+                                                            //but sadly I can't statically type in Javascript :(
+        var currentChart = { //Object which holds data on current chart, modify using setter methods
+            type: null,
+            data: null//This'll be an object within object
+        };
+        //console.log(currentChart.data);
         var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, { //PlaceHolder Chart
             type: 'bar',
@@ -46,11 +65,14 @@
             }
         });
         function changeChartType(chartType) {
-            myChart.destroy();
+            /*myChart.destroy();
             myChart = new Chart(ctx, {
                 type: chartType
-            });
+            });*/
+            currentChart.data = chartType;
+            console.log(currentChart.data);
         }
+        //SOURCE: https://stackoverflow.com/questions/1484506/random-color-generator
         function getRandomColor() {
             var letters = '0123456789ABCDEF';
             var color = '#';
@@ -58,6 +80,9 @@
                 color += letters[Math.floor(Math.random() * 16)];
             }
             return color;
+        }
+        function addOutlet(outletName) {
+
         }
         </script>
     </div>
