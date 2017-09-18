889d65b7c47fd5ca597eabb0bc3a4146a9683b93
ijm7 <ijmorton500@gmail.com>
2017-09-18 10:12:39 +0100

    charts controller now retrieves data from other controllers

--- app/Http/Controllers/ChartsController.php
+++ app/Http/Controllers/ChartsController.php
@@ -8,6 +8,9 @@ class chartsController extends Controller
 {
     public function index()
 	{
-		return view('charts.index');
+		$outlets = app('App\Http\Controllers\OutletsController')->index();
+		$customers = app('App\Http\Controllers\CustomersController')->index();
+		$transactions = app('App\Http\Controllers\TransactionsController')->index();
+		return view('charts.index', compact('outlets', 'customers', 'transactions'));
 	}
 }
