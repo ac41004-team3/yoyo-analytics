d49f4acadd2d6501153b2781fef237ac6a6c0ddf
ijm7 <ijmorton500@gmail.com>
2017-09-22 17:27:28 +0100

    Added tribes

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/charts', 'ChartsController@index');
+Route::get('/outlets', 'OutletsController@index');
