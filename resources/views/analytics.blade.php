3780c8efb58e2b4a49b677fa10ab4e00d7db021c
dbrer <dbrereton1995@gmail.com>
2017-09-22 14:52:39 +0100

    re-organised image page layout. graph now appears beside icons.

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -5,132 +5,141 @@
 <div class="container-fluid">
     
     <div class="row">
-        <div class="col-sm-10 col-sm-offset-2">     
-            <p><h1><u>Charts</u>:</h1> Select your chart</p>
+        <div class="col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">Your Chart</div>
+                <div class="panel-body">chart goes here</div>
+            </div>
+        </div>
+     
+        <div class="col-sm-4">
+        
         <div class="row">
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/charts/line-graphic.svg')}}">
+        <div class="col-sm-12">
+            <h4><b>Chart Selection</b></h4>
+        <div class="row">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/charts/line-graphic.svg')}}">
                 <p>Line</p>
             </div>
             
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/charts/bar-chart.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/charts/bar-chart.svg')}}">
                 <p>Bar</p>
             </div>
                 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/charts/pie-chart.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/charts/pie-chart.svg')}}">
                 <p>Pie</p> 
             </div>
             
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/charts/abc.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/charts/abc.svg')}}">
                 <p>Spatial</p>
             </div>
         </div>
         </div>
-    </div>
+        </div>
     
     </br>
     
     <div class="row">
-        <div class="col-sm-10 col-sm-offset-2">
-            
-            <p><h1><u>Options</u>:</h1> Customise your chart</p>
+        <div class="col-sm-12">  
             
          <div class="row">
              <div class="col-sm-12">
-                 <p><b>Outlets</b></p>
+                 <h4><b>Outlet Selection</b></h4>
              </div>                   
             
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/floorfive.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/floorfive.svg')}}">
                 <p>Floor Five</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/air_bar.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/air_bar.svg')}}">
                 <p>Air Bar</p>
             </div>  
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/foodonfour.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/foodonfour.svg')}}">
               <p>Food on Four</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/liar_bar.svg')}}">  
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/liar_bar.svg')}}">  
               <p>Liar Bar</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/mono.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/mono.svg')}}">
                 <p>Mono</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/reception.svg')}}">  
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/reception.svg')}}">  
               <p>Level 2 Reception</p>
             </div>
 
              
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/dusa_marketplace.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/dusa_marketplace.svg')}}">
                 <p>DUSA Marketplace</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/library.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/library.svg')}}">
                 <p>Library</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/outlets/cafe.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/outlets/cafe.svg')}}">
                 <p>Dental Café</p>
             </div>            
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/djcad_cantina.svg')}}">  
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/djcad_cantina.svg')}}">  
               <p>DJCAD Cantina</p>
             </div>
              
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/ninewells.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/ninewells.svg')}}">
               <p>Ninewells Shop</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/premier.svg')}}">  
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/premier.svg')}}">  
               <p>Premier Shop</p>
             </div>
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-              <img src="{{ URL::asset('/images/outlets/collegeshop.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+              <img class="img-fluid" src="{{ URL::asset('/images/outlets/collegeshop.svg')}}">
               <p>College Shop</p>
             </div>      
              
          </div>
-            </br>
+            
             <div class="row">
                 
              <div class="col-sm-12">
-                 <p><b>Tribes</b></p>
+                 <h4><b>Tribe Selection</b></h4>
              </div>
                 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/tribes/whale.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/tribes/whale.svg')}}">
                 <p>Whales</p>
             </div>
             
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/tribes/chicken.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/tribes/chicken.svg')}}">
                 <p>Early Birds</p>
             </div>
                 
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/tribes/owlface.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/tribes/owlface.svg')}}">
                 <p>Night Owls</p>  
             </div>
             
-            <div class="col-xs-4 col-sm-3 col-md-2 col-lg-2 selectionBox">
-                <img src="{{ URL::asset('/images/tribes/boss.svg')}}">
+            <div class="col-xs-4 col-sm-2 col-md-2 col-lg-2 selectionBox">
+                <img class="img-fluid" src="{{ URL::asset('/images/tribes/boss.svg')}}">
                 <p>Creatures of Habit</p> 
             </div>
          </div>
             
         </div>
     </div>
-    
+        </div>
+    </div>
     
 </div>
 
