1039c202ec50c6050600afe5cee76e60d921d2f9
James Neill <jneill542@gmail.com>
2017-09-27 00:02:51 +0100

    Updated layout to reflect it's previous style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,85 +24,81 @@
 <body>
 <div id="app">
     <div class="navbar navbar-fixed-top navbar-default" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <!-- collapsed hamburger -->
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('images/logo.png') }}" alt="DUSA Logo">
-                </a>
-            </div>
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <ul class="nav navbar-nav linkHover">
+        <div class="navbar-header">
+            <!-- collapsed hamburger -->
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ url('/') }}">
+                <img src="{{ asset('images/logo.png') }}" alt="DUSA Logo">
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <ul class="nav navbar-nav linkHover">
+                <li>
+                    <a href="{{route('home')}}">
+                        <span class="glyphicon glyphicon-dashboard">&nbsp;</span>Dashboard
+                    </a>
+                </li>
+                <li>
+                    <a href="{{route('analytics.index')}}">
+                        <span class="glyphicon glyphicon-signal">&nbsp;</span>Analytics
+                    </a>
+                </li>
+            </ul>
+            <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right linkHover">
+                @if(Gate::check('manage-users') || Gate::check('manage-assigned-users'))
                     <li>
-                        <a href="{{route('home')}}">
-                            <span class="glyphicon glyphicon-dashboard">&nbsp;</span>Dashboard
+                        <a href="{{route('admin.users.index')}}">
+                            <span class="glyphicon glyphicon-user">&nbsp;</span>Manage Users
                         </a>
                     </li>
+                @endif
+                @can('manage-data')
                     <li>
-                        <a href="{{route('analytics.index')}}">
-                            <span class="glyphicon glyphicon-signal">&nbsp;</span>Analytics
+                        <a href="{{route('admin.import.index')}}">
+                            <span class="glyphicon glyphicon-upload">&nbsp;</span>Upload
                         </a>
                     </li>
-                </ul>
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right linkHover">
-                    @if(Gate::check('manage-users') || Gate::check('manage-assigned-users'))
-                        <li>
-                            <a href="{{route('admin.users.index')}}">
-                                <span class="glyphicon glyphicon-user">&nbsp;</span>Manage Users
-                            </a>
-                        </li>
-                    @endif
-                    @can('manage-data')
-                        <li>
-                            <a href="{{route('admin.import.index')}}">
-                                <span class="glyphicon glyphicon-upload">&nbsp;</span>Upload
-                            </a>
-                        </li>
-                    @endcan
-                <!-- Authentication Links -->
-                    @guest
-                        <li><a href="{{ route('login') }}">Login</a></li>
-                        <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
-                                   aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{route('settings.index')}}"><span
-                                                    class="glyphicon glyphicon-cog"></span> Settings</a>
-                                    </li>
-                                    <li class="nav-divider"></li>
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                            <span class="glyphicon glyphicon-log-out"></span>
-                                            Logout
-                                        </a>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
+                @endcan
+            <!-- Authentication Links -->
+                @guest
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                           aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li>
+                                <a href="{{route('settings.index')}}"><span
+                                            class="glyphicon glyphicon-cog"></span> Settings</a>
                             </li>
-                            @endguest
-                </ul>
-            </div>
+                            <li class="nav-divider"></li>
+                            <li>
+                                <a href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                    <span class="glyphicon glyphicon-log-out"></span>
+                                    Logout
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                      style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @endguest
+            </ul>
         </div>
     </div>
-    <div class="container">
-        @yield('content')
-    </div>
+    @yield('content')
 </div>
 <script src="{{ asset('js/app.js') }}"></script>
 @yield('scripts')
